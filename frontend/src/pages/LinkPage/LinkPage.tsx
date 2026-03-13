b16162d4d4b003af2f3c4603c9903f7f361aa0ca
Erkinbekov Bilim <erkinbekov298@gmail.com>
2026-03-13 17:00:52 +0600

    feat(frontend): add link results display and polish styles

--- frontend/src/pages/LinkPage/LinkPage.tsx
+++ frontend/src/pages/LinkPage/LinkPage.tsx
@@ -2,18 +2,42 @@ import LinkForm from '../../entities/link/ui/LinkForm';
 import { useLinkStore } from '../../entities/link/model/link.store';
 import type { ISourceLink } from '../../entities/link/types/link.types';
 import styles from './LinkPage.module.css';
+import { API_URL } from '../../constants/constants';
 
 const LinkPage = () => {
-  const { postLink } = useLinkStore();
+  const { postLink, linkData } = useLinkStore();
 
   const handleSendLink = async (data: ISourceLink): Promise<void> => {
     await postLink(data);
+    console.log(linkData);
+  };
+
+  const renderContent = () => {
+    if (linkData) {
+      return (
+        <>
+          <p className={styles.linkTitle}>your link now looks like this:</p>
+          <a
+            className={styles.link}
+            target="_blank"
+            href={linkData.originalUrl}
+          >{`${API_URL}/${linkData.shortUrl}`}</a>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <p className={styles.linkTitle}>Enter the link you want to shorten</p>
+      </>
+    );
   };
 
   return (
     <div>
       <div className={styles.linkContent}>
         <LinkForm handleSendLink={handleSendLink} />
+        <div className={styles.linkBlock}>{renderContent()}</div>
       </div>
     </div>
   );
